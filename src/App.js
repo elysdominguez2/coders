79aeb6d16d58eb8e3c6a9740946593e3ab4e65fc
elysdominguez <elysdominguez2@gmail.com>
2022-09-20 20:38:05 +0200

    create PostPage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/post/:id" element={<PostPage />} />
       </Routes>
     </div>
   );
---
+++ src/pages/PostPage/index.js
@@ -0,0 +1,9 @@
+function PostPage() {
+  return (
+    <div>
+      <h1>Hello from PostPage</h1>
+    </div>
+  );
+}
+
+export default PostPage;
